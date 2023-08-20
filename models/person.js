f45d951266a887959a3af4f08ef1696ed025c6ef
Dmitri9149 <dmitri.as.name@gmail.com>
2023-08-20 13:19:38 +0300

    Add validation format for the phone numbers

--- models/person.js
+++ models/person.js
@@ -15,7 +15,17 @@ mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
   name: {type: String, minlength: 3, required: true},
-  number: {type:String, minlength: 8 }
+  number: {
+    type:String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /(\d{3\}-\d+)|(\d{2\}-\d+)/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number! The valid number has structure: XX-XXXX... or XXX-XXXX...`
+    },
+    required: [true, 'User phone number required']
+   }
 })
 
 personSchema.set('toJSON', {
