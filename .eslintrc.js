a662651df18baa0f48befa43fe2666eea7fdd527
Dmitri9149 <dmitri.as.name@gmail.com>
2023-08-20 13:59:04 +0300

    More eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,26 @@ module.exports = {
     indent: [
       'error',
       2
-    ]
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+  ],
+  'quotes': [
+      'error',
+      'single'
+  ],
+  'semi': [
+      'error',
+      'never'
+  ],
+  'eqeqeq': 'error',
+  'no-trailing-spaces': 'error',
+  'object-curly-spacing': [
+      'error', 'always'
+  ],
+  'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+  ]
   }
 }
